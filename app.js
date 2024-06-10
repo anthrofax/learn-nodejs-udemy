3eb7a6245ac86d0f5372b2098eda230bc0faeaf0
anthrofax <afridhoikhsan@gmail.com>
2024-06-11 00:35:04 +0700

    Understanding requests, Sending Responses

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url);
+  console.log(req.method)
+  console.log(req.headers)
+
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head>')
+  res.write('<title>My first page</title>')
+  res.write('<body><h1>Hello Node js!</h1></body>')
+  res.write('</title>')
+  res.write('</html>')
+  res.end()
 });
 
 server.listen(3000);
+
