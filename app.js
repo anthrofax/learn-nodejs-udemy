35258660215f1c179eb706f06d8cc392becc7bcf
anthrofax <afridhoikhsan@gmail.com>
2024-06-13 17:22:33 +0700

    Returning a 404 page

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -13,11 +16,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
