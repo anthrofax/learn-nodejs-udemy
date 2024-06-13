b551d2ee85a3849d3248f2d71388e9332b5cf948
anthrofax <afridhoikhsan@gmail.com>
2024-06-13 19:51:26 +0700

    Serving file statically

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   console.log("This always run");
