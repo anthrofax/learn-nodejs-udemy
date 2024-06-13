48133cc5ef532e1b0c0855cdc6143f155bdaafb8
anthrofax <afridhoikhsan@gmail.com>
2024-06-13 16:34:01 +0700

    Using express router

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express();
 
@@ -11,20 +13,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='product' method='POST'><input name='namaProduk' /><button>Tambah produk</button></form>"
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello Express!</h1>");
-});
+app.use(adminRoutes)
+app.use(shopRoutes);
 
 app.listen(3000);
