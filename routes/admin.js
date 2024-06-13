c0dd371f11086fc597866687810f71d82aa94297
anthrofax <afridhoikhsan@gmail.com>
2024-06-13 17:17:13 +0700

    Serving HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
+const path = require('path')
 const express = require("express");
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/admin/add-product' method='POST'><input name='namaProduk' /><button>Tambah produk</button></form>"
-  );
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 });
 
 router.post("/add-product", (req, res, next) => {
