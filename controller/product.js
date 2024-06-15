557a62a33644205ec55ca3675ac232218e7a578d
anthrofax <afridhoikhsan@gmail.com>
2024-06-15 16:28:03 +0700

    Storing Data in Files Via the Model

--- controller/product.js
+++ controller/product.js
@@ -4,14 +4,12 @@ exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
     title: "App | Add Product",
     path: "add-product",
-    activeAddProduct: true,
-    formCSS: true,
-    productCSS: true,
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
   const newProduct = new Product(req.body.title);
+
   newProduct.save();
 
   res.redirect("/");
@@ -19,13 +17,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   const product = Product.fetchAllProduct();
-
-  console.log(product)
+  
   res.render("shop", {
     title: "App | Shop",
     product,
     path: "shop",
-    hasProduct: product.length > 0,
-    activeShop: true,
   });
 };
--- models/product.js
+++ models/product.js
@@ -1,15 +1,34 @@
-const products = []
+const path = require("path");
+const fs = require("fs");
+
+const dataFilePath = path.join(require.main.path, "data", "product.json");
 
 module.exports = class Product {
-    constructor(title) {
-        this.title = title;
-    }
+  constructor(title) {
+    this.title = title;
+  }
 
-    save() {
-        products.push(this);
-    }
+  save() {
+    fs.readFile(dataFilePath, null, (err, fileContent) => {
+      let products = [];
 
-    static fetchAllProduct() {
-        return products
-    }
-}
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+
+      products.push(this);
+
+      fs.writeFile(dataFilePath, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAllProduct() {
+    fs.readFile(dataFilePath, null, (err, fileContent) => {
+      if (err) return [];
+
+      return JSON.parse(fileContent)
+    });
+  }
+};
