6cc132cd7f7ee99ece908e025411a718d1551741
anthrofax <afridhoikhsan@gmail.com>
2024-06-16 10:41:10 +0700

    136 - 143 (SQL Introduction)

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const db = require("./helpers/db");
+db.execute("SELECT * FROM products").then((result) => console.log(result[0]));
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
