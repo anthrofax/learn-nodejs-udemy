667aab31906b9e1ca14e58dcba53bca383cb0dc1
anthrofax <afridhoikhsan@gmail.com>
2024-06-18 21:46:31 +0700

    Inserting Data & Creating a Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
 
-  product.save().then(() => {
-    res.redirect("/");
-  });
+  Product.create({
+    title,
+    imageUrl,
+    price,
+    description,
+  })
+    .then((result) => {
+      res.redirect("/admin/products");
+      console.log(result);
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
