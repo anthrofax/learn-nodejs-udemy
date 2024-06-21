2c214baf91386d7675ba0f1abaea28b2c8f6b72a
anthrofax <afridhoikhsan@gmail.com>
2024-06-21 17:12:50 +0700

    Creating & Managing a Dummy User

--- app.js
+++ app.js
@@ -18,17 +18,42 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) =>
+      console.log("Find first initialization user internal error: " + err)
+    );
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then(() => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user)
+      return User.create({
+        name: "Afridho Ikhsan",
+        email: "afridhoikhsan@gmail.com",
+      });
+
+    return user;
+  })
+  .then((result) => {
+    console.log(result);
     app.listen(3000);
   })
   .catch((err) => console.log(err));
