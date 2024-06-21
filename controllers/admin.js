81fe4b37df6ac9f9158c4d5362ebdc98c86527b9
anthrofax <afridhoikhsan@gmail.com>
2024-06-21 17:21:09 +0700

    Using Magic Association Methods #Cara 1

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,6 +19,7 @@ exports.postAddProduct = (req, res, next) => {
     imageUrl,
     price,
     description,
+    userId: req.user.id,
   })
     .then((result) => {
       res.redirect("/admin/products");
