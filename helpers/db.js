923a1e253a6a01c9be72af0106c764e3e5d45c06
anthrofax <afridhoikhsan@gmail.com>
2024-11-24 21:38:04 +0700

    181. Installing the MongoDB Driver

--- helpers/db.js
+++ helpers/db.js
@@ -1,8 +1,17 @@
-const { Sequelize } = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node_complete", "root", "", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const connection = (callback) => {
+  MongoClient.connect(
+    `mongodb+srv://anthrofax:Qz5JnvHh3ljaKMX4@nodejs-udemy-ecomerce.gmbku.mongodb.net/?retryWrites=true&w=majority&appName=nodejs-udemy-ecomerce`
+  )
+    .then((client) => {
+      console.log(client);
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+    });
+};
 
-module.exports = sequelize;
+module.exports = connection;
