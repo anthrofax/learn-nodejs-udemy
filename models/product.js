f108b6f99204ef2a82994875c72eb916d76abad0
anthrofax <afridhoikhsan@gmail.com>
2024-11-26 23:57:25 +0700

    182. Creating the Database Connection

--- models/product.js
+++ models/product.js
@@ -1,27 +1,38 @@
 const { DataTypes } = require("sequelize");
 
-const sequelize = require("../helpers/db");
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
 
-const Product = sequelize.define("product", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: DataTypes.STRING,
-  price: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  save() {
+
+  }
+}
+
+// const Product = sequelize.define("product", {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true,
+//   },
+//   title: DataTypes.STRING,
+//   price: {
+//     type: DataTypes.DOUBLE,
+//     allowNull: false,
+//   },
+//   imageUrl: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   description: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+// });
 
 module.exports = Product;
