1d87f7651fd424ff4a928eaf72b8af7c31bfbf12
anthrofax <afridhoikhsan@gmail.com>
2024-12-16 11:00:23 +0700

    234. Optional: Creating the Login Form

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const errorController = require("./controllers/error");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 const User = require("./models/user");
 
 const app = express();
@@ -31,6 +32,7 @@ app.use(async (req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
--- controller/error.js
+++
@@ -1,3 +0,0 @@
-exports.getError = (req, res) => {
-  res.render("404", { title: "Page Not Found", path: "" });
-};
