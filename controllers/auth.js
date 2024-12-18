6449616dd2f2fdbb5f1a24d9d8927ca2736534fc
anthrofax <afridhoikhsan@gmail.com>
2024-12-18 14:01:25 +0700

    244. Deleting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,3 +15,11 @@ exports.postLogin = (req, res) => {
   req.session.isLoggedIn = true;
   res.redirect("/");
 };
+
+exports.postLogout = (req, res) => {
+  req.session.destroy((err) => {
+    console.log(err);
+
+    res.redirect("/");
+  });
+};
