074cc1585034fa2cf0a29eccae5acbb1b136defd
anthrofax <afridhoikhsan@gmail.com>
2024-12-18 08:29:13 +0700

    238. Configuring Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 exports.getLogin = (req, res) => {
-  const isLoggedIn = req.get("Cookie")?.split(";")[0].trim().split(":")[1] === 'true';
-  console.log(isLoggedIn);
+  const isLoggedIn = req.get("Cookie")?.split(";")[0].trim().split("=")[1] === 'true';
+
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
@@ -9,6 +9,6 @@ exports.getLogin = (req, res) => {
 };
 
 exports.postLogin = (req, res) => {
-  res.setHeader("Set-Cookie", "isLoggedIn:false");
+  res.setHeader("Set-Cookie", "isLoggedIn=true; HttpOnly");
   res.redirect("/");
 };
